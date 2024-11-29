4be7a2f942d9ae1592a517c3cd8aba42fd669c85
Prashant Malviya <90743489+Prashant-Malviya@users.noreply.github.com>
2024-11-29 12:26:41 +0530

    Update main.py

--- Weather App/main.py
+++ Weather App/main.py
@@ -5,7 +5,7 @@ import os
 city = input("enter the name of the city \n")
 
 
-url = f"http://api.weatherapi.com/v1/current.json?key=1b6567ea4e384b49b63171300242611&q={city}"
+url = f"http://api.weatherapi.com/v1/current.json?key=key&q={city}"
 
 r = requests.get(url)
 
